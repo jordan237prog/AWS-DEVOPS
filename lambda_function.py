ab654feb0eb56c165e51fa00d836d95d775bb8d7
JORDAN <lordmoyojordan@gmail.com>
2021-10-21 21:29:36 +0200

    first commit, the lambda function returns all the availability zones

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+# PIPLINESTACK = s3 + sqs + lambda
+
+import boto3
+import json
+
+ec2 = boto3.client('ec2')
+
+def lambda_handler(event, context):
+  response = ec2.describe_availability_zones()
+  return {"statusCode": 200, "body":json.dumps(response)}
